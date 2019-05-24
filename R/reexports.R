475fac8a32683c5cd58622a4a26ffd080232ac08
Arsenij Ustjanzew <Arsenij.Ustjanzew@mpi-bn.mpg.de>
2019-05-24 15:49:09 +0200

    added plotly_barplot function to visualization_functions; redone the barplot.R script; redone the barplot.Rmd: one dataframe will be generated with a function in dependence of the if cases.

---
+++ R/reexports.R
@@ -0,0 +1,15 @@
+#' magrittr forward-pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}}.
+#' @name %>%
+#' @importFrom magrittr %>%
+#' @export %>%
+NULL
+
+#' magrittr forward-backward-pipe operator
+#'
+#' See \code{\link[magrittr]{\%<>\%}}.
+#' @name %<>%
+#' @importFrom magrittr %<>%
+#' @export %<>%
+NULL
