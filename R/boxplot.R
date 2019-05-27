07775413459fd2a9adc0ba67cc4514be1f43b3e4
Arsenij Ustjanzew <Arsenij.Ustjanzew@mpi-bn.mpg.de>
2019-05-27 12:57:42 +0200

    added title arguments of x and group_by; small changes in validation steps and description; naming the lists;
    adapted the part for statical report;
    added download option;
    combined reactives to one single reactive outputting a data frame
    
    adapted function to accept a dataframe as input and axestitles
    
    added reexport of magrittr pipes

--- R/boxplot.R
+++ R/boxplot.R
@@ -1,4 +1,4 @@
-#' Renders a boxplot for cluster characterization
+#' Renders a boxplot
 #'
 #' @param object A \linkS4class{i2dash::i2dashboard} object.
 #' @param x Numeric observations for the boxplot. In case of a named list, a dropdown menu will be provided in the interactive mode.
@@ -6,21 +6,26 @@
 #' @param title The title of the components junk.
 #'
 #' @return A string containing markdown code for the rendered component.
-#'
 #' @export
-boxplot <- function(object, x, group_by, title = NULL) {
+boxplot <- function(object, x, group_by = NULL, title = NULL, title_x = NULL, title_group_by = NULL) {
   # Create random env id
   env_id <- paste0("env_", stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
 
+  # Create list if element is not a list already
   if(!is.list(x)) x <- list(x)
-  if(!is.list(group_by)) group_by <- list(group_by)
+  if(!is.list(group_by) & !is.null(group_by)) group_by <- list(group_by)
+
+  # name the lists
+  if(is.null(names(group_by)) & !is.null(group_by)) group_by %<>% magrittr::set_names("group")
+  if(is.null(names(x))) x %<>% magrittr::set_names("sample")
 
   # Validate input
   if(!all(sapply(x, is.numeric))) stop("'x' should only contain numerical values.")
-  if(!all(sapply(cluster, is.factor))) stop("'cluster' should only contain factorial values.")
+  if(!all(sapply(group_by, is.factor)) & !is.null(group_by)) stop("'group_by' should only contain factorial values.")
 
   # Create component environment
   env <- new.env()
+
   env$x_selection <- FALSE
   env$group_by_selection <- FALSE
 
@@ -29,6 +34,9 @@ boxplot <- function(object, x, group_by, title = NULL) {
 
   env$group_by <- group_by
   env$group_by_selection <- length(env$group_by) > 1
+
+  env$title_x <- title_x
+  env$title_group_by <- title_group_by
 
   # save environment object
   saveRDS(env, file = file.path(object@workdir, "envs", paste0(env_id, ".rds")))
