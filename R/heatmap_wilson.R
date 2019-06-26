e1675b2e62ad6bfe761fda1debacc73e75def61e
Arsenij Ustjanzew <Arsenij.Ustjanzew@mpi-bn.mpg.de>
2019-06-26 10:02:54 +0200

    Implementation of create_heatmap(9 method from wilson. added reexports.R but this not work properly (?) rebuild the heatmap_wilson.Rmd and added several selection options

--- R/heatmap_wilson.R
+++ R/heatmap_wilson.R
@@ -8,7 +8,7 @@
 #'
 #' @return A string containing markdown code for the rendered textbox
 #' @export
-scatterplot_wilson <- function(object, countTable, group_by, title = NULL, ...) {
+heatmap_wilson <- function(object, countTable, group_by, title = NULL, ...) {
   # Create random env id
   env_id <- paste0("env_", stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
 
@@ -54,6 +54,6 @@ scatterplot_wilson <- function(object, countTable, group_by, title = NULL, ...) 
 
   # Expand component
   timestamp <- Sys.time()
-  expanded_component <- knitr::knit_expand(file = system.file("templates", "scatterplot_wilson.Rmd", package = "i2dash.scrnaseq"), title = title, env_id = env_id, date = timestamp)
+  expanded_component <- knitr::knit_expand(file = system.file("templates", "heatmap_wilson.Rmd", package = "i2dash.scrnaseq"), title = title, env_id = env_id, date = timestamp)
   return(expanded_component)
 }
