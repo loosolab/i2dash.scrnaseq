38c450e99e5e7aefa80be24251620490726f8e5f
Arsenij Ustjanzew <Arsenij.Ustjanzew@mpi-bn.mpg.de>
2019-07-17 12:39:03 +0200

    Implementation of a dimension reduction page in statical mode

---
+++ R/dimension_reduction_page.R
@@ -0,0 +1,81 @@
+#' Renders a dimension reduction page
+#'
+#' @param object A \linkS4class{i2dash::i2dashboard} object.
+#' @param reduced_dim A data.frame or matrix containing two columns for the x and y coordinates. The rownames are used as labels.
+#' @param metadata A data.frame or matrix containing a maximum of four columns with cell metadata (e.g. cluster, timepoint, number of genes, etc). Should have the same rownumber as 'reduced_dim'. Note: Factorial metadata needs to be of class factor in the data.frame.
+#' @param highlight A character string identical to one of the column names in 'metadata'.
+#' @param title The title of the page.
+#' @param labels (Optional) A vector with labels. Should be of the same length as the rownumber of 'reduced_dim'.
+#' @param barplot_highlight (Default: TRUE) A barplot with the number of observations in 'highlight' will be created.
+#' @param menu (Optional) The name of the menu, under which the page should appear.
+#' @param sidebar (Optional) The page layout (see below).
+#'
+#' @return A string containing markdown code for the rendered page.
+#' @export
+add_dim_reduction_page <- function(object, reduced_dim, metadata, highlight, title, labels = NULL, barplot_highlight = TRUE, menu = NULL, sidebar = NULL) {
+
+
+
+  # Create random env id
+  env_id <- paste0("env_", stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
+
+  # Validate input
+  if(!is.data.frame(reduced_dim) & !is.matrix(reduced_dim)) stop("'reduced_dim' should should be of class 'data.frame' or 'matrix'.")
+  if(!dim(reduced_dim)[[2]] == 2 ) stop("'reduced_dim' should contain two columns.")
+  if(!is.data.frame(metadata) & !is.matrix(metadata)) stop("'metadata' should be of class 'data.frame' or 'matrix'.")
+  if(is.null(colnames(metadata))) stop("'metadata' should contain colnames.")
+  if(!dim(metadata)[[2]] < 5 ) stop("'metadata' should contain 4 columns at maximum.")
+  if(dim(metadata)[[1]] != dim(reduced_dim)[[1]]) stop("'metadata' and 'reduced_dim' should contain the same number of rows.")
+  if(!highlight %in% colnames(metadata)) stop("'metadata' does not contain a column with this name.")
+  if(is.factor(metadata$highlight)) stop("'highlight' in 'metadata' should contain factors and be of class 'factor'.")
+  if(!is.character(title)) stop("'title' should be a character vector.")
+  if(!is.null(labels) & length(labels) != dim(reduced_dim)[[1]]) stop("The length of the vector 'labels' should be equal to the number of rows in 'reduced_dim'.")
+
+  comp_nr <- dim(metadata)[[2]]
+
+  # Create component environment
+  env <- new.env()
+
+  env$reduced_dim <- reduced_dim
+  env$metadata <- metadata
+  env$highlight <- highlight
+  env$labels <- labels
+  #env$barplot_highlight <- barplot_highlight
+
+  # save environment object
+  saveRDS(env, file = file.path(object@workdir, "envs", paste0(env_id, ".rds")))
+  expanded_components <- list()
+
+  timestamp <- Sys.time()
+  #dim_reduction <- knitr::knit_expand(file = system.file("templates", "boxplot.Rmd", package = "i2dash.scrnaseq"), title = title, env_id = env_id, date = timestamp)
+  # fill list "expanded_components" with components
+  max_comp <- 0
+  dim_reduction <- knitr::knit_expand(file = system.file("templates", "red_dim_1.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
+  expanded_components <- append(expanded_components, dim_reduction)
+  max_comp <- max_comp + 1
+  if(barplot_highlight){
+    barplot_highlight_component <- knitr::knit_expand(file = system.file("templates", "red_dim_2.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
+    expanded_components <- append(expanded_components, barplot_highlight_component)
+    max_comp <- max_comp + 1
+  }
+
+  highlight_index <- which(colnames(metadata) == highlight)
+  meta_names <- colnames(metadata)[-highlight_index]
+  for(i in meta_names){
+    if(max_comp < 4){
+      meta_component <- knitr::knit_expand(file = system.file("templates", "red_dim_3.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp, meta_column = i)
+      expanded_components <- append(expanded_components, meta_component)
+      max_comp <- max_comp + 1
+    }
+  }
+
+
+  # Expand component
+  timestamp <- Sys.time()
+  expanded_component <- knitr::knit_expand(file = system.file("templates", "boxplot.Rmd", package = "i2dash.scrnaseq"), title = title, env_id = env_id, date = timestamp)
+
+  object@pages[["dim_reduction_page"]] <- list(title = title, layout = "2x2_grid", menu = menu, components = expanded_components, max_components = 4, sidebar = sidebar)
+  return(object)
+  #return(list("title" = title, "layout" = "2x2_grid", menu = NULL, components = expanded_components))
+
+}
