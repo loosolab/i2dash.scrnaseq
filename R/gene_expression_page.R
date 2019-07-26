506ae615962e1ab456dbca921923fe8feb80df50
jens.preussner <jens.preussner@mpi-bn.mpg.de>
2019-07-26 16:46:32 +0200

    Apply suggestion to R/gene_expression_page.R

--- R/gene_expression_page.R
+++ R/gene_expression_page.R
@@ -26,7 +26,7 @@ add_gene_expression_page <- function(object, reduced_dim, count_table, metadata,
   if(nrow(metadata) != nrow(reduced_dim)) stop("'metadata' and 'reduced_dim' should contain the same number of rows.")
   if(!grouping %in% colnames(metadata)) stop("'metadata' does not contain a column with this name.")
   if(!is.null(title) & !is.character(title)) stop("'title' should be a character vector.")
-  if(!is.null(labels) & length(labels) != dim(reduced_dim)[[1]]) stop("The length of the vector 'labels' should be equal to the number of rows in 'reduced_dim'.")
+  if(!is.null(labels) & length(labels) != nrow(reduced_dim)) stop("The length of the vector 'labels' should be equal to the number of rows in 'reduced_dim'.")
 
   # Create component environment
   env <- new.env()
