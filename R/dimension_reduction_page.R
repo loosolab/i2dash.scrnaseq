85b0f90d0bd56d805a47b8b792898f19c94265ed
Arsenij Ustjanzew <Arsenij.Ustjanzew@mpi-bn.mpg.de>
2019-08-05 15:31:16 +0200

    added converting metadata to a data.frame; tested the recursive call for speed.

--- R/dimension_reduction_page.R
+++ R/dimension_reduction_page.R
@@ -102,12 +102,11 @@ setMethod("add_dim_reduction_page",
 setMethod("add_dim_reduction_page",
           signature = signature(object = "i2dashboard", sc_object = "SingleCellExperiment"),
           function(object, sc_object, reduced_dim, metadata, grouping, title = NULL, labels = NULL, barplot_grouping = TRUE, silhouette_plot = FALSE, menu = NULL, sidebar = NULL) {
-
+            beginning <- Sys.time()
             # validate and extract metadata
             if(!is.character(metadata) & !is.list(metadata)) stop("'metadata' should be a character or a list.")
             if(!all(metadata %in% names(colData(sc_object)))) stop("'colData' slot of the SingleCellExperiment object does not contain the column names from 'metadata'.")
-            metadata <- sc_object@colData[metadata]
-
+            metadata <- as.data.frame(sc_object@colData[metadata])
             # validate input and extract dimension reduction
             if(!is.character(reduced_dim) | length(reduced_dim) > 1) stop("'reduced_dim' should be a character of length 1.")
             if(!reduced_dim %in% reducedDimNames(sc_object)) stop("'reducedDim' slot of the SingleCellExperiment object does not contain the name provided in 'reduced_dim'.")
@@ -154,7 +153,8 @@ setMethod("add_dim_reduction_page",
             max_comp <- max_comp + 1
             if(barplot_grouping){
               barplot_grouping_component <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_barplot_grouping.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
-              expanded_components <- append(expanded_components, barplot_grouping_component)
+              expanded_components <- append(expanded_comp
+                                            onents, barplot_grouping_component)
               max_comp <- max_comp + 1
             }
 
@@ -178,7 +178,23 @@ setMethod("add_dim_reduction_page",
             timestamp <- Sys.time()
 
             object@pages[["dim_reduction_page"]] <- list(title = title, layout = "2x2_grid", menu = menu, components = expanded_components, max_components = 4, sidebar = sidebar)
+            end <- Sys.time()
+            print(end - beginning)
             return(object)
+            # object <- add_dim_reduction_page(object = object,
+            #                                    reduced_dim = reduced_dim,
+            #                                    metadata = metadata,
+            #                                    grouping = grouping,
+            #                                    title = title,
+            #                                    labels = labels,
+            #                                    menu = menu,
+            #                                    sidebar = sidebar,
+            #                                  barplot_grouping = barplot_grouping,
+            #                                  silhouette_plot = silhouette_plot)
+            # end <- Sys.time()
+            # print(end - beginning)
+            # return(object)
+
 })
 
 #' Renders a page with four linked components at maximum. The first component is a scatterplot, showing samples in along coordinates from \code{reduced_dim}. If \code{barplot_grouping} is \code{TRUE} the second component will be a bar plot, that shows the number of observations by groups defined in \code{grouping}. If \code{silhouette_plot} is \code{TRUE} the third component will be a silhoutte plot, that shows how well each observation has been classified. Therefore the Euclidean distance is applied to the coordinates from \code{reduced_dim} and grouped by the groups defined in \code{grouping}. The remaining component slots are filled with bar or boxplots, that visualize one of the columns from \code{metadata} by groups defined in \code{grouping}.
@@ -276,6 +292,8 @@ setMethod("add_dim_reduction_page",
 
             object@pages[["dim_reduction_page"]] <- list(title = title, layout = "2x2_grid", menu = menu, components = expanded_components, max_components = 4, sidebar = sidebar)
             return(object)
+
+
 })
 
 
