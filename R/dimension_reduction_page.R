7aeee091d4c7046ce7ee8e08ef0eea10300dc2b5
Arsenij Ustjanzew <Arsenij.Ustjanzew@mpi-bn.mpg.de>
2019-08-07 10:55:31 +0200

    Deleted labels in manpage description; shortened the code through recursive call

--- R/dimension_reduction_page.R
+++ R/dimension_reduction_page.R
@@ -91,7 +91,6 @@ setMethod("add_dim_reduction_page",
 #' @param metadata A character or list representing the names of  columns of \code{colData}.
 #' @param grouping A character of length 1 representing the name of columns of \code{colData} and used for expression grouping.
 #' @param title The title of the page.
-#' @param labels (Optional) A vector with labels. Should be of the same length as the rownumber of \code{reduced_dim}.
 #' @param barplot_grouping (Optional) A logical value. (Default: TRUE) If TRUE, a barplot with the number of observations from 'grouping' will be created.
 #' @param silhouette_plot (Optional) A logical value. (Default: FALSE) If TRUE, a silhouette plot will be created.
 #' @param menu (Optional) The name of the menu, under which the page should appear in the navigation.
@@ -101,7 +100,7 @@ setMethod("add_dim_reduction_page",
 #' @export
 setMethod("add_dim_reduction_page",
           signature = signature(object = "i2dashboard", sc_object = "SingleCellExperiment"),
-          function(object, sc_object, reduced_dim, metadata, grouping, title = NULL, labels = NULL, barplot_grouping = TRUE, silhouette_plot = FALSE, menu = NULL, sidebar = NULL) {
+          function(object, sc_object, reduced_dim, metadata, grouping, title = NULL, barplot_grouping = TRUE, silhouette_plot = FALSE, menu = NULL, sidebar = NULL) {
             # validate and extract metadata
             if(!is.character(metadata) & !is.list(metadata)) stop("'metadata' should be a character or a list.")
             if(!all(metadata %in% names(colData(sc_object)))) stop("'colData' slot of the SingleCellExperiment object does not contain the column names from 'metadata'.")
@@ -116,81 +115,17 @@ setMethod("add_dim_reduction_page",
 
             labels <- colnames(sc_object)
 
-            # Create random env id
-            env_id <- paste0("env_", stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
-
-            # Validate input
-            # if(!is.data.frame(reduced_dim) & !is.matrix(reduced_dim)) stop("'reduced_dim' should should be of class 'data.frame' or 'matrix'.")
-            # if(ncol(reduced_dim) < 2 ) stop("'reduced_dim' should contain at least two columns.")
-            # if(!is.data.frame(metadata) & !is.matrix(metadata)) stop("'metadata' should be of class 'data.frame' or 'matrix'.")
-            # if(is.null(colnames(metadata))) stop("'metadata' should contain colnames.")
-            # if(ncol(metadata) > 4 ) stop("'metadata' should contain 4 columns at maximum.")
-            # if(nrow(metadata) != nrow(reduced_dim)) stop("'metadata' and 'reduced_dim' should contain the same number of rows.")
-            # if(!grouping %in% colnames(metadata)) stop("'metadata' does not contain a column with this name.")
-            # if(is.factor(metadata$grouping)) stop("'grouping' in 'metadata' should contain factors and be of class 'factor'.")
-            # if(!is.null(title) & !is.character(title)) stop("'title' should be a character vector.")
-            # if(!is.null(labels) & length(labels) != nrow(reduced_dim)) stop("The length of the vector 'labels' should be equal to the number of rows in 'reduced_dim'.")
-
-            # Create component environment
-            env <- new.env()
-            env$reduced_dim <- reduced_dim[, 1:2]
-            env$metadata <- metadata
-            env$grouping <- grouping
-            env$labels <- labels
-            env$multiple_meta <- ncol(metadata) > 1
-
-            # save environment object
-            saveRDS(env, file = file.path(object@workdir, "envs", paste0(env_id, ".rds")))
-
-            expanded_components <- list()
-            timestamp <- Sys.time()
-
-            # fill list "expanded_components" with components
-            max_comp <- 0
-            dim_reduction <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_dimred.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
-            expanded_components <- append(expanded_components, dim_reduction)
-            max_comp <- max_comp + 1
-            if(barplot_grouping){
-              barplot_grouping_component <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_barplot_grouping.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
-              expanded_components <- append(expanded_comp
-                                            onents, barplot_grouping_component)
-              max_comp <- max_comp + 1
-            }
-
-            if(silhouette_plot){
-              silhouette_plot_component <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_silhouette.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
-              expanded_components <- append(expanded_components, silhouette_plot_component)
-              max_comp <- max_comp + 1
-            }
-
-            grouping_index <- which(colnames(metadata) == grouping)
-            meta_names <- colnames(metadata)[-grouping_index]
-            for(i in meta_names){
-              if(max_comp < 4){
-                meta_component <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_barplot_boxplot.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp, meta_column = i)
-                expanded_components <- append(expanded_components, meta_component)
-                max_comp <- max_comp + 1
-              }
-            }
-
-            # Expand component
-            timestamp <- Sys.time()
-
-            object@pages[["dim_reduction_page"]] <- list(title = title, layout = "2x2_grid", menu = menu, components = expanded_components, max_components = 4, sidebar = sidebar)
+            object <- add_dim_reduction_page(object = object,
+                                             reduced_dim = reduced_dim,
+                                             metadata = metadata,
+                                             grouping = grouping,
+                                             title = title,
+                                             labels = labels,
+                                             menu = menu,
+                                             sidebar = sidebar,
+                                             barplot_grouping = barplot_grouping,
+                                             silhouette_plot = silhouette_plot)
             return(object)
-            # object <- add_dim_reduction_page(object = object,
-            #                                    reduced_dim = reduced_dim,
-            #                                    metadata = metadata,
-            #                                    grouping = grouping,
-            #                                    title = title,
-            #                                    labels = labels,
-            #                                    menu = menu,
-            #                                    sidebar = sidebar,
-            #                                  barplot_grouping = barplot_grouping,
-            #                                  silhouette_plot = silhouette_plot)
-            # end <- Sys.time()
-            # print(end - beginning)
-            # return(object)
 
 })
 
@@ -228,69 +163,17 @@ setMethod("add_dim_reduction_page",
 
             labels <- colnames(sc_object)
 
-            # Create random env id
-            env_id <- paste0("env_", stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
-
-            # Validate input
-            # if(!is.data.frame(reduced_dim) & !is.matrix(reduced_dim)) stop("'reduced_dim' should should be of class 'data.frame' or 'matrix'.")
-            # if(ncol(reduced_dim) < 2 ) stop("'reduced_dim' should contain at least two columns.")
-            # if(!is.data.frame(metadata) & !is.matrix(metadata)) stop("'metadata' should be of class 'data.frame' or 'matrix'.")
-            # if(is.null(colnames(metadata))) stop("'metadata' should contain colnames.")
-            # if(ncol(metadata) > 4 ) stop("'metadata' should contain 4 columns at maximum.")
-            # if(nrow(metadata) != nrow(reduced_dim)) stop("'metadata' and 'reduced_dim' should contain the same number of rows.")
-            # if(!grouping %in% colnames(metadata)) stop("'metadata' does not contain a column with this name.")
-            # if(is.factor(metadata$grouping)) stop("'grouping' in 'metadata' should contain factors and be of class 'factor'.")
-            # if(!is.null(title) & !is.character(title)) stop("'title' should be a character vector.")
-            # if(!is.null(labels) & length(labels) != nrow(reduced_dim)) stop("The length of the vector 'labels' should be equal to the number of rows in 'reduced_dim'.")
-
-            # Create component environment
-            env <- new.env()
-            env$reduced_dim <- reduced_dim
-            env$metadata <- metadata
-            env$grouping <- grouping
-            env$labels <- labels
-            env$multiple_meta <- ncol(metadata) > 1
-
-            # save environment object
-            saveRDS(env, file = file.path(object@workdir, "envs", paste0(env_id, ".rds")))
-
-            expanded_components <- list()
-            timestamp <- Sys.time()
-
-            # fill list "expanded_components" with components
-            max_comp <- 0
-            dim_reduction <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_dimred.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
-            expanded_components <- append(expanded_components, dim_reduction)
-            max_comp <- max_comp + 1
-            if(barplot_grouping){
-              barplot_grouping_component <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_barplot_grouping.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
-              expanded_components <- append(expanded_components, barplot_grouping_component)
-              max_comp <- max_comp + 1
-            }
-
-            if(silhouette_plot){
-              silhouette_plot_component <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_silhouette.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp)
-              expanded_components <- append(expanded_components, silhouette_plot_component)
-              max_comp <- max_comp + 1
-            }
-
-            grouping_index <- which(colnames(metadata) == grouping)
-            meta_names <- colnames(metadata)[-grouping_index]
-            for(i in meta_names){
-              if(max_comp < 4){
-                meta_component <- knitr::knit_expand(file = system.file("templates", "dimension_reduction_barplot_boxplot.Rmd", package = "i2dash.scrnaseq"), env_id = env_id, date = timestamp, meta_column = i)
-                expanded_components <- append(expanded_components, meta_component)
-                max_comp <- max_comp + 1
-              }
-            }
-
-            # Expand component
-            timestamp <- Sys.time()
-
-            object@pages[["dim_reduction_page"]] <- list(title = title, layout = "2x2_grid", menu = menu, components = expanded_components, max_components = 4, sidebar = sidebar)
+            object <- add_dim_reduction_page(object = object,
+                                             reduced_dim = reduced_dim,
+                                             metadata = metadata,
+                                             grouping = grouping,
+                                             title = title,
+                                             labels = labels,
+                                             menu = menu,
+                                             sidebar = sidebar,
+                                             barplot_grouping = barplot_grouping,
+                                             silhouette_plot = silhouette_plot)
             return(object)
-
-
 })
 
 
