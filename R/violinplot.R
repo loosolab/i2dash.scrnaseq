09cb95a9a68c0d844ce825b50e73bd59472f129c
Arsenij Ustjanzew <Arsenij.Ustjanzew@mpi-bn.mpg.de>
2019-08-29 14:10:55 +0200

    general overhaul of code; implementation of wrapper for SingleCellExperiment

--- R/violinplot.R
+++ R/violinplot.R
@@ -1,52 +1,114 @@
-#' Renders a vertical violin plot
+#' Renders a component containing a vertical violin plot
 #'
-#' @param object A \linkS4class{i2dash::i2dashboard} object.
-#' @param y A vector with numerical values or a named list, which represents the observations for the vertical violinplot (y-axis). In case of a named list, a dropdown menu will be provided in the interactive mode.
-#' @param group_by (Optional) A vector with factorial values or characters or a named list, which will be used for grouping the observations. In case of a named list, a dropdown menu will be provided in the interactive mode.
+#' @name violinplot
+#' @rdname violinplot
+#' @exportMethod violinplot
+setGeneric("violinplot", function(object, sc_object, ...) standardGeneric("violinplot"))
+
+#'
+#' @rdname violinplot
+#' @param object An object of class \linkS4class{i2dash::i2dashboard}.
+#' @param y A data.frame or matrix with columns containing numeric observations for the vertical violinplot (y-axis).
+#' @param group_by (Optional) A data.frame or matrix with columns conatining factors for grouping the observations.
 #' @param title (Optional) The title of the components junk.
-#' @param title_y (Optional) The title of the y-axis that describes the observations. In case of a named list this parameter is not needed because the names of the list will be used as title of the y axis.
-#' @param title_group_by (Optional) The title of the x-axis that describes the grouping factor. In case of a named list this parameter is not needed because the names of the list will be used as title of the x axis.
 #'
 #' @return A string containing markdown code for the rendered component
-violinplot <- function(object, y, group_by = NULL,  title = NULL, title_y = NULL, title_group_by = NULL) {
-  # Create random env id
-  env_id <- paste0("env_", stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
+setMethod("violinplot",
+          signature = signature(object = "i2dashboard", sc_object = "missing"),
+          function(object, y, group_by = NULL,  title = NULL) {
+            # Create random env id
+            env_id <- paste0("env_", stringi::stri_rand_strings(1, 6, pattern = "[A-Za-z0-9]"))
 
-  # Create list if element is not a list already
-  if(!is.list(y)) y <- list(y = y)
-  if(!is.list(group_by) & !is.null(group_by)) group_by  <- list(group_by)
+            # Validate input
+            assertive.types::assert_is_any_of(y, c("data.frame", "matrix"))
+            if(is.null(colnames(y))) stop("'y' does not have colnames.")
+            if(!is.null(group_by)){
+              assertive.types::assert_is_any_of(group_by, c("data.frame", "matrix"))
+              if(is.null(colnames(group_by))) stop("'group_by' does not have colnames.")
+              if(nrow(y) != nrow(group_by)) stop("The numbers of rows in 'y' and 'group_by' are not equal.")
+            }
 
-  # Validate input
-  if(!all(sapply(y, is.numeric))) stop("'y' should only contain numerical values.")
+            # Create component environment
+            env <- new.env()
 
-  if(any(sapply(group_by, is.character)) & !is.null(group_by)){
-    clust_names <- names(group_by[sapply(group_by, class) == 'character'])
-    for (name in clust_names){
-      group_by[[name]] <- as.factor(group_by[[name]])
-    }
-  }
-  if(!all(sapply(group_by, is.factor)) & !is.null(group_by)) stop("'group_by' should only contain factorial values.")
+            env$y <- y
+            env$y_selection <- length(y) > 1
 
-  # Create component environment
-  env <- new.env()
+            env$group_by <- group_by
+            env$group_by_selection <- length(group_by) > 1
 
-  env$y_selection <- FALSE
-  env$group_by_selection <- FALSE
+            # save environment object
+            saveRDS(env, file = file.path(object@datadir, paste0(env_id, ".rds")))
 
-  env$y <- y
-  env$y_selection <- length(env$y) > 1
+            # Expand component
+            timestamp <- Sys.time()
+            expanded_component <- knitr::knit_expand(file = system.file("templates", "violinplot.Rmd", package = "i2dash.scrnaseq"), title = title, env_id = env_id, date = timestamp)
+            return(expanded_component)
+          })
 
-  env$group_by <- group_by
-  env$group_by_selection <- length(env$group_by) > 1
+#' @rdname violinplot
+#'
+#' @param sc_object A valid \linkS4class{SingleCellExperiment::SingleCellExperiment} object.
+#' @param use A character specifying where to obtain the data from. Valid input: "colData" or "rowData".
+#' @param y A character vector with column names to use for observations.
+#' @param group_by A character vector with column names to use for grouping the observations.
+#'
+#' @inheritParams violinplot,i2dashboard,missing-method
+#'
+#' @return An object of class \linkS4class{i2dash::i2dashboard}.
+#' @export
+setMethod("violinplot",
+          signature = signature(object = "i2dashboard", sc_object = "SingleCellExperiment"),
+          function(object, sc_object, use = "colData", y = NULL, group_by = NULL,  title = NULL) {
 
-  env$title_y <- title_y
-  env$title_group_by <- title_group_by
+            if(use == "colData"){
+              #
+              # definte y data.frame
+              #
+              if(!is.null(y)){
+                assertive.sets::assert_is_subset(y, colnames(SummarizedExperiment::colData(sc_object)))
+                SummarizedExperiment::colData(sc_object) %>%
+                  as.data.frame() %>%
+                  dplyr::select(!!y) -> y
+              } else {
+                SummarizedExperiment::colData(sc_object) %>%
+                  as.data.frame() -> y
+              }
+              #
+              # definte group_by data.frame
+              #
+              if(!is.null(group_by)){
+                assertive.sets::assert_is_subset(group_by, colnames(SummarizedExperiment::colData(sc_object)))
+                SummarizedExperiment::colData(sc_object) %>%
+                  as.data.frame() %>%
+                  dplyr::select(!!group_by) -> group_by
+              }
+            } else if (use == "rowData"){
+              #
+              # definte y data.frame
+              #
+              if(!is.null(y)){
+                assertive.sets::assert_is_subset(y, colnames(SummarizedExperiment::rowData(sc_object)))
+                SummarizedExperiment::rowData(sc_object) %>%
+                  as.data.frame() %>%
+                  dplyr::select(!!y) -> y
+              } else {
+                SummarizedExperiment::rowData(sc_object) %>%
+                  as.data.frame() -> y
+              }
+              #
+              # definte group_by data.frame
+              #
+              if(!is.null(group_by)){
+                assertive.sets::assert_is_subset(group_by, colnames(SummarizedExperiment::rowData(sc_object)))
+                SummarizedExperiment::rowData(sc_object) %>%
+                  as.data.frame() %>%
+                  dplyr::select(!!group_by) -> group_by
+              }
+            }
+            violinplot(object,
+                       y = y,
+                       group_by = group_by,
+                       title = title)
+          })
 
-  # save environment object
-  saveRDS(env, file = file.path(object@workdir, "envs", paste0(env_id, ".rds")))
-
-  # Expand component
-  timestamp <- Sys.time()
-  expanded_component <- knitr::knit_expand(file = system.file("templates", "violinplot.Rmd", package = "i2dash.scrnaseq"), title = title, env_id = env_id, date = timestamp)
-  return(expanded_component)
-}
--- R/visualization_functions.R
+++ R/visualization_functions.R
@@ -1,195 +1,23 @@
-#' Render a scatter plot
-#'
-#' @param labels A list with sample names, that should be of the same length as x and y.
-#' @param x Numeric values mapped to the x-axis. In case of a nested list, a dropdown menu will be provided in the interactive mode.
-#' @param y Numeric values mapped to the y-axis. In case of a nested list, a dropdown menu will be provided in the interactive mode.
-#' @param colour_by A factor that will be mapped to colours. In case of a nested list, a dropdown menu will be provided in the interactive mode.
-#'
-#' @return A list with 1. the plotly object & 2. the data frame used in the plot
-#' @export
-.scatter_plot <- function(labels = NULL, x, y, colour_by = NULL, checkbox = FALSE, selected_label = NULL){
-  # create data.frame for plot (fill colour_by & labels with dummy data if NULL)
-  dummy_values <- FALSE
-  plot_list <- list()
-  if (!is.null(labels)) plot_list <- rlist::list.append(plot_list, labels) else plot_list <- rlist::list.append(plot_list, c(1:length(x[[1]])))
-  plot_list <- rlist::list.append(plot_list, c(x, y))
-  if (!is.null(colour_by)) {
-    plot_list <- rlist::list.append(plot_list, colour_by)
-  } else {
-    plot_list <- rlist::list.append(plot_list, c(1:length(x[[1]])))
-    dummy_values <- TRUE
-  }
-  plot_df <- do.call("data.frame", plot_list)
-
-  # assign variables
-  labels <- plot_df[[1]]
-  x_value <- plot_df[[2]]
-  y_value <- plot_df[[3]]
-  colour_by <- plot_df[[4]]
-
-  x_title <- names(plot_df[2])
-  y_title <- names(plot_df[3])
-
-  if(dummy_values) {
-    colour_by <- NULL
-  }
-  # if(is.null(labels)) {
-  #   labels <- NULL
-  # }
-  if(is.null(checkbox)) {
-    checkbox <- FALSE
-  }
-
-  # find point for annotation
-  a <- NULL
-  if (!is.null(labels)) {
-    if (checkbox) {
-      colour_by <- NULL
-      point_index <- match(selected_label, plot_df[[1]])
-      point <- plot_df[point_index,]
-      a <- list(
-        x = point[,2],
-        y = point[,3],
-        text = point[,1],
-        xref = "x",
-        yref = "y",
-        showarrow = T,
-        arrowhead = 7,
-        arrowcolor = "red",
-        ax = 20,
-        ay = -40
-      )
-    }
-  }
-
-  # plotly
-  p <- plotly::plot_ly(plot_df,
-                       x = x_value,
-                       y = y_value,
-                       color = colour_by,
-                       text = labels
-  )
-  p <- plotly::add_markers(p)
-  p <- plotly::layout(p,
-                      xaxis = list(title = x_title),
-                      yaxis = list(title = y_title),
-                      annotations = a)
-  return(p)
-}
-
-#' Render a bar plot
-#'
-#' @param cluster Values for the membership to clusters. In case of a nested list, a dropdown menu will be provided in the interactive mode.
-#' @param x Numeric values mapped to the x-axis. In case of a nested list, a dropdown menu will be provided in the interactive mode.
-#'
-#' @return A list with 1. the plotly object & 2. the data frame used in the plot
-#' @export
-.bar_plot <- function(cluster, x = NULL){
-
-  #if x = NULL -> plot for "Number of cells"
-  #if x != NULL -> plot for "Number of fractions"
-  if(is.null(x)){
-    tab <- table(cluster)
-    tab_df <- as.data.frame(tab)
-
-    # plotly
-    title <- "Number of cells"
-    p <- plotly::plot_ly(tab_df, x = tab_df[[2]], y = tab_df[[1]],
-                         name = names(tab_df[1]),
-                         type = "bar", orientation = "h", opacity = 0.7)
-    p <- plotly::layout(p,
-                        xaxis = list(title=title, showline = T),
-                        yaxis = list(title="Cluster", showline = T, showticklabels = T),
-                        showlegend = F
-    )
-    return(list("plot" = p, "df" = tab_df))
-  } else {
-    # create data.frame for plot
-    tab <- table(cluster[[1]],x[[1]])
-    ptab <- prop.table(tab,margin = 1)
-    ptab_df <- as.data.frame.matrix(ptab)
-
-    # plotly
-    title <- "Fraction of cells"
-    p <- plotly::plot_ly(ptab_df, type = "bar", orientation = "h", opacity = 0.7)
-    for(i in 1:length(names(ptab_df))){
-      p <- plotly::add_trace(p, x = ptab_df[[i]], y = row.names(ptab_df), name = names(ptab_df[i]))
-    }
-    p <- plotly::layout(p,
-                        xaxis = list(title=title, showline = T),
-                        yaxis = list(title="Cluster", showline = T, showticklabels = T),
-                        barmode = 'stack',
-                        showlegend = T
-    )
-    return(list("plot" = p, "df" = ptab_df))
-  }
-
-}
-
-#' Render a box plot with plotly.
-#'
-#' @param x Numeric observations for the boxplot.
-#' @param group_by A factor, by which observations can optionally be grouped.
-#' @param title A title that describes the observations.
-#' @param group_by_title A title that describes the grouping factor.
-#'
-#' @return An object of class \code{plotly}.
-#' @export
-plotly_boxplot <- function(x, group_by = NULL, title = "", group_by_title = NULL){
-  plotly::plot_ly(x = x, y = group_by, type = "box", name = title) %>%
-    plotly::layout(xaxis = list(title = title, showline = T),
-                   yaxis = list(title = group_by_title, showline = T, showticklabels = T),
-                   showlegend = T)
-}
-
-#' Function to create a dataframe for plotly_violinplot.
-#'
-#' @param y A list with the observations for the violinplot.
-#' @param group_by (Optional) A list with factorial values, by which observations can optionally be grouped.
-#' @param title_y (Optional) The title of the y-axis that describes the observations.
-#' @param title_group_by (Optional) The title of the x-axis that describes the grouping factor.
-#'
-#' @return An object of class \code{list} containig the dataframe 'df', the vector 'x' with values for the x-axis, the vector 'y' with values for the y-axis, the vector 'split', the boolean value 'showlegend', the character string "title_y", the character string "title_group_by".
-#' @export
-create_violinplot_df <- function(y, group_by = NULL, title_y = NULL, title_group_by = NULL){
-  # create data_frame
-  if(is.null(group_by)){
-    df <- data.frame(y)
-  } else {
-    df <- data.frame(y, group_by)
-  }
-
-  # manage the titles of axis
-  if(is.null(title_y)) title_y <- names(df[1]) else title_y <- title_y
-  if(is.null(title_group_by) & !is.null(group_by)) title_group_by <- names(df[2]) else title_group_by <- title_group_by
-
-  # set variables in dependence of 'group_by'
-  if (is.null(group_by)) {
-    x <- NULL
-    showlegend <- F
-  } else {
-    x <- df[[2]]
-    showlegend <- T
-  }
-
-  return(list("df"=df, "x"=x, "y"=df[[1]], "split"=x, "showlegend"=showlegend, "title_y"=title_y, "title_group_by"=title_group_by))
-}
-
 #' Render a vertical violin plot with plotly.
 #'
-#' @param ... these arguments are of either the form value or tag = value and should be valid for the 'plotly::plot_ly()' method.
-#' @param showlegend Boolean value that describes if the legend should be shown.
-#' @param title_y (Optional) The title of the y-axis that describes the observations.
-#' @param title_group_by (Optional) The title of the x-axis that describes the grouping factor.
+#' @param df A data.frame containing in the first column numerical values for the y axis and optionally factor in the second column for grouping the observations along the x axis.
 #'
 #' @return An object of class \code{plotly}.
 #' @export
-plotly_violinplot <- function(..., showlegend = F, title_y = NULL, title_group_by = NULL){
-  plotly::plot_ly(..., type = 'violin',
-                  box = list(visible = T),
-                  meanline = list(visible = T)) %>%
-    plotly::layout(xaxis = list(title = title_group_by, showline = T),
-                   yaxis = list(title = title_y, showline = T),
-                   showlegend = showlegend)
+plotly_violinplot <- function(df){
+  if(ncol(df) > 1){
+    df %>%
+      plotly::plot_ly(colors = "Set1", x = df[,2], y = df[,1], color = df[,2], type = "violin", box = list(visible = T), meanline = list(visible = T), points = "all", jitter = 0) %>%
+      plotly::layout(
+        xaxis = list(title = colnames(df)[2]),
+        yaxis = list(title = colnames(df)[1])
+      )
+  } else {
+    df %>% plotly::plot_ly(colors = "Set1", x = NULL, y = df[,1], color = NULL, type = "violin", box = list(visible = T), meanline = list(visible = T), points = "all", jitter = 0) %>%
+      plotly::layout(
+        xaxis = list(title = NULL),
+        yaxis = list(title = colnames(df)[1])
+      )
+  }
 }
 
