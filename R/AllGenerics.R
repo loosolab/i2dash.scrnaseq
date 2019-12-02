f2d4dd96e37f888eb072e3117ea88086c0f15fb1
arsenij.ustjanzew <arsenij.ustjanzew@mpi-bn.mpg.de>
2019-12-02 12:57:02 +0100

    fixed colormap comparison in scatterplot; added plot_title to scatterplot; added column_title, row_title and option to show column labels to heatmap; removed double code from AllGenerics.R

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -191,27 +191,6 @@ setGeneric("boxplot", function(dashboard, object, ...) standardGeneric("boxplot"
 #' @exportMethod add_dimred_sample_page
 setGeneric("add_dimred_sample_page", function(dashboard, object, ...) standardGeneric("add_dimred_sample_page"))
 
-#' Renders a component containing a heatmap based on \link[ComplexHeatmap]{Heatmap}.
-#'
-#' @param dashboard An object of class \linkS4class{i2dash::i2dashboard}.
-#' @param object An object of class \linkS4class{Seurat::Seurat} or \linkS4class{SingleCellExperiment::SingleCellExperiment}.
-#' @param exprs_values A data.frame (matrix) containing expression data of features of interest in rows and samples in columns or a character indicating which assay of the \linkS4class{SingleCellExperiment::SingleCellExperiment} object to use.
-#' @param assay A character specifying the assay (\code{object@assays}) to obtain expression values from. (Default: "RNA")
-#' @param assay_slot A character specifying the name of the data slot in the assay. (Default: "data")
-#' @param subset_row A character vector (of feature names), a logical vector or numeric vector (of indices) specifying the features to use.
-#' @param column_split An optional data.frame (matrix) containing factorial metadata (e.g. cluster, timepoint, etc.) along samples for splitting the columns or a character vector indicating the columns to use from the "meta.data" of a Seurat \code{object}/ the columns to use from "colData" of a SingleCellExperiment. Enables a second visualisation option: heatmap with levels of \code{column_split} as columns and features of \code{exprs_values} as rows.
-#' @param visualisation_mode Used in case of the static mode and if \code{column_split} is provided: Select between "splitted" or "summarized" heatmap visualisation according to the levels in the 1. column of \code{column_split}.
-#' @param title Title of the component.
-#' @param cluster_rows A logical controls whether to make cluster on rows.
-#' @param cluster_columns A logical controls whether to make cluster on columns.
-#' @param clustering_distance A pre-defined character which is in ("euclidean", "maximum", "manhattan", "binary", "minkowski").
-#' @param clustering_method Method to perform hierarchical clustering, pass to \link[stats]{hclust} ("average", "ward.D", "ward.D2", "single", "complete", "mcquitty", "median","centroid").
-#'
-#' @name heatmap
-#' @rdname heatmap
-#' @exportMethod heatmap
-setGeneric("heatmap", function(dashboard, object, ...) standardGeneric("heatmap"))
-
 #' Renders a component containing a scatterplot with optional selection options
 #'
 #' @param dashboard An object of class \linkS4class{i2dash::i2dashboard}.
@@ -228,6 +207,7 @@ setGeneric("heatmap", function(dashboard, object, ...) standardGeneric("heatmap"
 #' @param title The title of the components junk.
 #' @param x_title An optional title of the x-axis. If not provided the column names from \code{x} are used instead.
 #' @param y_title An optional title of the y-axis. If not provided the column names from \code{y}  are used instead.
+#' @param plot_title An optional title of the plot.
 #'
 #' @details The parameters \code{x}, \code{y}, \code{colour_by}, \code{use}, \code{use_dimred}, \code{exprs_values}, \code{assay} and \code{slot}) take different arguments depending on the class of \code{object}.
 #'   In case no object is supplied (\emph{i2dashboard,missing}-method), the parameters \code{x}, \code{y}, \code{colour_by} and \code{exprs_values} are expected to be of class \code{data.frame} or \code{matrix}. T he parameters \code{x}, \code{y} can also be a numeric vector. The parameters \code{use}, \code{use_dimred}, \code{assay} and \code{slot} can be ignored.
@@ -266,6 +246,9 @@ setGeneric("scatterplot", function(dashboard, object, ...) standardGeneric("scat
 #' @param cluster_columns A logical controls whether to make cluster on columns.
 #' @param clustering_method Method to perform hierarchical clustering, passed to \link[stats]{hclust}.
 #' @param clustering_distance The distance measure to use for hierarchical clustering.
+#' @param show_column_labels A logical controls whether the column lables should be displayed. Pay attention that a large number of columns with column lables can cause visualization problems.
+#' @param column_title The column title of the heatmap.
+#' @param row_title The row title of the heatmap.
 #'
 #' @name heatmap
 #' @rdname heatmap
